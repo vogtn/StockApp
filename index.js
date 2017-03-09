fff9a9752e471d3ce70392edd2a00b888de67ca0
vogtn <nicolasjvogt@gmail.com>
2017-03-09 13:43:15 -0800

    working auth

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ var db = require('./models');
 var app = express();
 var request = require('request');
 require("dotenv").config();
+var SlackStrategy = require('passport-slack').Strategy;
+var passport = require('passport');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,31 +14,60 @@ app.use(ejsLayouts);
 
 
 
+
+
+//Passport setup
+passport.use(new SlackStrategy({
+    clientID: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET
+  }, (accessToken, refreshToken, profile, done) => {
+    done(null, profile);
+  }
+));
+ 
+ //session
+var session = require('express-session');
+app.use(session({secret: "nicv"}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.serializeUser(function(user, done) {
+  // placeholder for custom user serialization
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  // placeholder for custom user deserialization.
+  // user by id?
+  done(null, user);
+});
+
+
 app.get('/', function(req,res){
     res.render('main/login');
 })
 
-app.get('/slack', function(req,res){
-    if(!req.query.code){
-        res.send('Youve been denied!');
-        return;
-    }
-    var data = {form: {
-      client_id: process.env.SLACK_CLIENT_ID,
-      client_secret: process.env.SLACK_CLIENT_SECRET,
-      code: req.query.code
-  }};
-  request.post('https://slack.com/api/oauth.access', data, function (error, response, body){
-      if(!error && response.statusCode == 200){
-          let teamId = JSON.parse(body).team.id;
-          res.redirect('/portfolio');
-      }else{
-          res.redirect('/');
-      }
-  })
-})
 
-app.use('/portfolio', require('./controllers/portfolio'));
+app.get('/auth/slack/', passport.authorize('slack'));
+ 
+app.get('/auth/slack/callback',
+  passport.authenticate('slack', { failureRedirect: '/' }),
+  (req, res) => res.redirect('/portfolio') // Successful authentication, redirect to portfolio. 
+);
+
+
+//check if authentication
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/')
+}
+
+app.get('/protected', ensureAuthenticated, function(req, res) {
+  res.send("access granted");
+});
+
+app.use('/portfolio', ensureAuthenticated, require('./controllers/portfolio'));
 
 var server = app.listen(process.env.PORT || 3000);
 
