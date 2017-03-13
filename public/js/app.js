fe2e5aee90e1c9f9658b865c25f46e67d868e851
vogtn <nicolasjvogt@gmail.com>
2017-03-13 01:55:50 -0700

    final touches

--- public/js/app.js
+++ public/js/app.js
@@ -9,36 +9,10 @@ $( document ).ready(function() {
     $(".char6").hide().fadeIn(6000);
     $(".char7").hide().fadeIn(6000);
 
-var data = $("#data").text()
-// set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+var data = d3.csvParseRows($("#data").text())
+if(data.length > 1){
 
-// parse the date / time
-var parseTime = d3.timeParse("%d-%b-%y");
-
-// set the ranges
-var x = d3.scaleTime().range([0, width]);
-var y = d3.scaleLinear().range([height, 0]);
-
-// define the line
-var valueline = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
-
-// append the svg obgect to the body of the page
-// appends a 'group' element to 'svg'
-// moves the 'group' element to the top left margin
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
-
-
-
+}
 
 
     //d3 table
